68bbd967716e2bb68bbf07d12f81119ee6f33ba5
Fraud Man <gachaman703@gmail.com>
2024-11-16 00:12:33 +0100

    Root Files

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_water.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_water-user \
+    omni_water-userdebug \
+    omni_water-eng
